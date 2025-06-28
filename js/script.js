404bfa8a55619040772d42243fa73c5e4b8c0f3b
nafij-islam <sahariannafis70@gmail.com>
2025-06-28 23:06:50 +0600

    update

--- js/script.js
+++ js/script.js
@@ -5,13 +5,55 @@ let convertHeading=Array.from(heading)
 
 
 
+
 convertHeading.map(item=>{
     
     let count=0
     function counterjs(){
-    count++
     item.innerHTML=count
-    if (count==50) {
+    switch (count) {
+        case 1:
+            item.style.color='red'
+            break;
+         case 2:
+            item.style.color='green'
+            break;
+        case 3:
+            item.style.color='blue'
+            break;
+        case 4:
+            item.style.color='#c100ff'
+            break;
+
+        case 5:
+            item.style.color='blue'
+            break;
+         
+        case 6:
+            item.style.color='#f3ff00'
+            break;
+
+        case 7:
+            item.style.color='blue'
+            break;
+
+        case 8:
+            item.style.color='#f3ff00'
+            break;
+
+        case 9:
+            item.style.color='#f3ff00'
+            break;
+
+        case 10:
+            item.style.color='blue'
+            break;
+
+    }
+
+
+    count++
+    if (count>item.dataset.number) {
     clearInterval(stop)
     
 }
@@ -21,7 +63,7 @@ let stop =setInterval(() => {
  
 counterjs()
     
-}, 100);
+},item.dataset.speed)
 
 
 })
